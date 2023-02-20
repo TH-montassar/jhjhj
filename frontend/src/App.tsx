650124b310b6429a9a4f53dbffe83b6499d18d56
Montassar <montassar.themri1@gmail.com>
2023-02-20 06:33:53 +0100

    init frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
